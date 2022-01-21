c9b5e33a95f086a0fb7d5cfbe7b4ed284046b5ad
Josselin Milon <josselinmilon@MacBook-Pro-de-Josselin-2.local>
2022-01-21 20:13:28 +0100

    initial commit

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const formidable = require("express-formidable");
+const mongoose = require("mongoose");
+require('dotenv').config;
+
+const app = express();
+app.use(formidable());
+
+mongoose.connect(process.env.MONGO_DB_URI);
+
+// Importing Task
+const Task = require("./models/Task");
+
+app.post("/add-task", async (req, res) => {
+    try{
+        console.log(req.fields.task)
+        const newTask = new Task({
+            task: req.fields.task
+        });
+        await newTask.save();
+        res.status(200).json(`${req.fields.task} added`);
+    }
+    catch(error) {
+        res.status(400).json({error: error.message});
+    }
+});
+
+app.all("*", (req, res) => {
+    res.status(404).json({error: "page not found"})
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log("Server has started...")
+});
