8e5e57e3a1e7d3f8b8c5c6450bf13c6f3e5ae72f
Josselin Milon <josselinmilon@MacBook-Pro-de-Josselin-2.local>
2022-01-23 20:07:11 +0100

    updated the / route to return all tasks

--- server.js
+++ server.js
@@ -13,8 +13,15 @@ mongoose.connect(process.env.MONGO_DB_URI || "mongodb://localhost/reacttodolist"
 // Importing Task
 const Task = require("./models/Task");
 
-app.get("/", (req, res) => {
-    res.status(200).json("Hello world")
+app.get("/", async (req, res) => {
+    try {
+        const taskArray = await Task.find()
+        const response = taskArray.map((item) => {return item.task});
+        res.status(200).json(response)
+    }
+    catch(error) {
+        res.status(400).json({error: error.message})
+    }
 });
 
 app.post("/add-task", async (req, res) => {
